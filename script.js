823560770209b02cb38132602f16922ffee70e19
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-04-22 12:53:03 -0300

    feat: code refactoring

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let products = [
   {
     item: 'Teclado Gamer REDRAGON',
     description: 'Gaming RGB wired USB Keyboard',
-    quantity: 150,
+    quantity: 250,
     price: 332.9,
     image: 'https://cdn.pichau.com.br/wysiwyg/Descricao/Pichau/PGK-P421-RGB/PGK-P421-RGB7.png',
     id: 1,
@@ -26,7 +26,7 @@ let products = [
   {
     item: 'Porta Copos',
     description: 'Porta copos com tema do Simpsons',
-    quantity: 50,
+    quantity: 250,
     price: 49.90,
     image: 'https://images.tcdn.com.br/img/img_prod/437561/570_1_20161019154633.jpg',
     id: 3,
@@ -55,7 +55,7 @@ let products = [
 
   {
     item: 'Webcam Logitech HD',
-    description: 'Webcam Full HD Logitech StreamCam Plus',
+    description: 'Webcam Full HD Logitech Plus',
     quantity: 250,
     price: 759.90,
     image: 'https://www.logitechstore.com.br/media/catalog/product/cache/1/image/634x545/9df78eab33525d08d6e5fb8d27136e95/s/t/streamcam.png',
@@ -64,187 +64,143 @@ let products = [
   },
 
   {
-    item: 'Mouse Gamer REDRAGON',
-    description: 'Headset Gamer Fallen Morcego Surround Virtual 7.1',
+    item: 'Headset Gamer Fallen 7.1',
+    description: 'Headset Gamer Fallen Morcego',
     quantity: 250,
     price: 699.90,
     image: 'https://static.cdnlive.com.br/uploads/602/produto/16148186668419_zoom.jpg',
     id: 7,
+    selected: false
   },
 ];
-
-let inputId;
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById('myBtn');
 var span = document.getElementsByClassName('close')[0];
 
-function createHTMLforProducts(product) {
-  product.price = product.price.toString().replace('.', ',')
-
-  const productDiv = document.createElement('div');
-  productDiv.id = 'product-div'
-
-  const itemNameTag = document.createElement('h2');
-  const itemNameText = document.createTextNode(product.item);
-  const itemDescriptionTag = document.createElement('p');
-  const itemDescriptionText = document.createTextNode(product.description);
-
-  const imageTag = document.createElement("img")
-  imageTag.src = product.image
-
-  const paymentDiv = document.createElement('div');
-  const itemPriceTag = document.createElement('h5');
-  const itemPriceDescription = document.createElement('p')
-  itemPriceDescription.innerHTML = 'R$'
-
-  itemPriceTag.appendChild(itemPriceDescription)
-  const itemPriceText = document.createTextNode(product.price);
-
-  const inputHidden = document.createElement('input');
-  inputHidden.type = 'hidden';
-  inputHidden.value = product.id;
-
-  const form = document.createElement('form');
-  form.id = 'submit';
-
-  const button = document.createElement('button');
-  button.innerHTML = 'COMPRAR';
-
-  form.appendChild(inputHidden);
-  form.appendChild(button);
-
-  productDiv.append(imageTag)
-  itemNameTag.appendChild(itemNameText);
-  productDiv.appendChild(itemNameTag);
-  itemDescriptionTag.appendChild(itemDescriptionText);
-  productDiv.appendChild(itemDescriptionTag);
-
-  itemPriceTag.appendChild(itemPriceText);
-  paymentDiv.appendChild(itemPriceTag);
-  productDiv.appendChild(paymentDiv);
-  productDiv.appendChild(form);
-
-  document.getElementById('products-container').appendChild(productDiv);
-}
-
-products.map((product, index) => {
-  createHTMLforProducts(product)
-});
-
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  inputId = +e.srcElement.childNodes[0].value;
-
-  const existingProduct = products.filter((item) => item.id === inputId);
-
-  if (!existingProduct) {
-    alert('Product does not exists, refresh the page and try again');
-    return null
-  }
-
-  const quantity = carts.products.length;
-
-  existingProduct[0].quantity -= 1
-  carts.products.push(existingProduct[0]);
-  carts.quantity = quantity + 1
-
-  modal.style.display = 'block';
-
-  console.log(carts)
-
-  generateHTMLCartShop()
-});
 
 document.getElementById("search-input").onkeydown = (e) => {
   const searchString = e.target.value.toLowerCase();
 
-  const filteredProducts = products.filter(product => {
-    return (
-      product.item.toLowerCase().includes(searchString)
-    )
-  });
+  const filteredProducts = products.filter(product => product.item.toLowerCase().includes(searchString));
 
-  const existingProductsHTML = document.querySelectorAll("#product-div");
-  existingProductsHTML.forEach(product => {
-    product.remove()
-  })
+  document.getElementById("products-container").innerHTML = '';
 
-  filteredProducts.map(product => {
-    createHTMLforProducts(product)
-  })
-
+  filteredProducts.map(product => { createHTMLforProducts(product) })
 }
 
-
-span.onclick = function () {
+span.onclick = () => {
   modal.style.display = 'none';
 };
 
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+function createHTMLforProducts(product) {
+  product.price = product.price.toString().replace('.', ',')
 
-function generateHTMLCartShop() {
+  const productItem =
+    `
+    <div id='product-div'>
+      <img src=${product.image}>
+      <h2>${product.item}</h2>
+      <p>${product.description}</p>
+      <div>
+        <h5><p>RS</p>${product.price}</h5>
+      </div>
+      <form id="submit">
+        <input type="hidden" value=${product.id}>
+        <button>COMPRAR</button>
+      </form>
+    </div>
+  `
+
+  document.getElementById('products-container').innerHTML += productItem;
+}
+
+function createHTMLcartShop(regenerate = false) {
   carts.products.map((product) => {
-    if (!product.selected) {
-      const cartDiv = document.createElement('div');
-      cartDiv.id = 'cart-div'
-
-      const productImageDiv = document.createElement('div')
-      productImageDiv.id = 'product-image-div'
-
-      const productSectionDiv = document.createElement('div')
-      productSectionDiv.id = 'product-section-div'
-
-      const image = document.createElement('img')
-      image.src = product.image
-
-      productImageDiv.appendChild(image)
-
-      const productTitle = document.createElement("h1")
-      productTitle.innerHTML = product.item
-
-      const productDescription = document.createElement("p")
-      productDescription.innerHTML = product.description
-
-      const productQuantity = document.createElement("p")
-      productQuantity.innerHTML = 'Quantidade: ' + product.quantity
-
-      const inputHidden = document.createElement('input');
-      inputHidden.type = 'hidden';
-      inputHidden.value = product.id;
-
-      const buttonAdd = document.createElement("button")
-      buttonAdd.innerHTML = '+';
-      buttonAdd.id = 'add'
-
-      const buttonSubtract = document.createElement("button")
-      buttonSubtract.innerHTML = '-'
-      buttonSubtract.id = 'subtract'
-
-      productSectionDiv.appendChild(productTitle)
-      productSectionDiv.appendChild(productDescription)
-      productSectionDiv.appendChild(productQuantity)
-      productSectionDiv.appendChild(buttonAdd)
-      productSectionDiv.appendChild(buttonSubtract)
-      productSectionDiv.appendChild(inputHidden)
-
-      cartDiv.appendChild(productImageDiv)
-      cartDiv.appendChild(productSectionDiv)
-
-      const priceText = document.createElement('p')
-
-      priceText.innerHTML = 'R$ ' + product.price
-
-      cartDiv.appendChild(priceText)
-
-      document.getElementById("cart-products-container").appendChild(cartDiv)
+    if (!product.selected && !regenerate) {
+      const cartItem =
+        `
+        <div id='cart-div'>
+          <div id='product-image-div'>
+            <img src=${product.image} >
+          </div>
+          <div id='product-section-div'>
+            <h1>${product.item}<h1>
+            <h2>${product.description}</h1>
+            <p>quantidade restante: <span id="product-quantity${product.id}">${product.quantity}</span></p>
+            <p>quantidade no carrinho: <span id="user-quantity${product.id}">1</span></p>
+            <div class="forms">
+              <form id="add">
+                <button value="-" id="add">+</button>
+                <input type="hidden" value=${product.id}>
+              </form>
+              <form id="subtract">
+                <button value="-" id="subtract">-</button>
+                <input type="hidden" value=${product.id}>
+              </form>
+            </div>
+          </div>
+          <h2 id="product-price${product.id}">R$: ${product.price}</h2>
+        </div>
+      `
       product.selected = true
+
+      document.getElementById('cart-products-container').innerHTML += cartItem;
     }
   })
 }
+
+products.map(product => createHTMLforProducts(product))
+
+function getProductById(e, i) {
+  let inputId = +e.srcElement.childNodes[i].value;
+
+  const existingProduct = products.filter((item) => item.id === inputId);
+
+  if (!existingProduct) alert('Product out of range')
+
+  return existingProduct
+}
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (e.target.id == 'submit') {
+    const existingProduct = getProductById(e, 1)
+    const quantity = carts.products.length;
+
+    existingProduct[0].quantity -= 1
+    carts.products.push(existingProduct[0]);
+    carts.quantity = quantity + 1
+
+    modal.style.display = 'block';
+
+    createHTMLcartShop()
+
+  } else if (e.target.id == 'add') {
+    const existingProduct = getProductById(e, 3)
+
+    if (existingProduct[0].quantity > 0) {
+      existingProduct[0].quantity -= 1
+      document.getElementById(`product-price${existingProduct[0].id}`).innerHTML
+      = 'R$: ' + (Math.round(+existingProduct[0].price.replace(',', '.'), 4) * (250 - existingProduct[0].quantity))
+    }
+    
+    document.getElementById(`product-quantity${existingProduct[0].id}`).innerHTML = (existingProduct[0].quantity)
+    document.getElementById(`user-quantity${existingProduct[0].id}`).innerHTML = (250 - existingProduct[0].quantity)
+
+
+  } else if (e.target.id == 'subtract') {
+    const existingProduct = getProductById(e, 3)
+
+    if (existingProduct[0].quantity < 250) {
+      existingProduct[0].quantity += 1
+      document.getElementById(`product-price${existingProduct[0].id}`).innerHTML
+      = 'R$: ' + (Math.round(+existingProduct[0].price.replace(',', '.'), 4) * (250 - existingProduct[0].quantity))
+    }
+
+    
+    document.getElementById(`product-quantity${existingProduct[0].id}`).innerHTML = (existingProduct[0].quantity)
+    document.getElementById(`user-quantity${existingProduct[0].id}`).innerHTML = (250 - existingProduct[0].quantity)
+  }
+});
